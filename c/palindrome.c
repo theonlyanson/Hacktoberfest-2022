923ce221d6d05b1a593d47ecbeab9acd5a7468fd
Priyanshu shekhar <106399466+Priyanshushekhar77@users.noreply.github.com>
2022-10-23 19:02:10 +0530

    Update palindrome.c
    checking palindrome of a number using recursive method.
    thanks for accepting my request

--- c/palindrome.c
+++ c/palindrome.c
@@ -1,19 +1,24 @@
-#include<stdio.h>  
-int main()    
-{    
-int n,r,sum=0,temp;    
-printf("enter the number=");    
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
-{    
-r=n%10;    
-sum=(sum*10)+r;    
-n=n/10;    
-}    
-if(temp==sum)    
-printf("palindrome number ");    
-else    
-printf("not palindrome");   
-return 0;  
-}   
+#include<stdio.h> 
+int getReverse(int num, int rev){
+    if(num == 0)
+        return rev;
+    
+    int rem = num % 10;
+    rev = rev * 10 + rem;
+    
+    return getReverse(num / 10, rev);
+}
+
+int main ()
+{
+    int num, reverse = 0;
+    scanf("%d",&num);
+    printf("The number is: %d\n",num);
+        
+    
+    if (num == getReverse(num, reverse))
+        printf("%d is Palindrome\n", num);
+    else
+        printf("%d is Not Palindrome\n", num);
+
+}
